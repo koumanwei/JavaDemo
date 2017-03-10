a84759c6547a88a5d3578fe6a6b971fa98c8b7ba
koumanwei <874899973@qq.com>
2017-03-10 23:51:51 +0800

    调整代码

--- src/com/koumanwei/exception/CustomException.java
+++ src/com/koumanwei/exception/CustomException.java
@@ -1,11 +1,11 @@
 package com.koumanwei.exception;
 
-public class CustomException extends Exception{
-	 public  CustomException(String msg) {
-		 // 调用父类的构造方法
-		 // 正常我们throw一个空指针异常的时候，在其构造函数中传入字符串就能有信息
-		 // 其实内部就是调用了父类的构造方法
-		 super(msg);
+public class CustomException extends Exception {
+	public CustomException(String msg) {
+		// 调用父类的构造方法
+		// 正常我们throw一个空指针异常的时候，在其构造函数中传入字符串就能有信息
+		// 其实内部就是调用了父类的构造方法
+		super(msg);
 	}
 
 	public void CustomException() {
