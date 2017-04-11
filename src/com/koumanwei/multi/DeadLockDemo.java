db7f283c743389eb562f811b2143a7e77a6cac1d
koumanwei <874899973@qq.com>
2017-04-12 00:33:03 +0800

    增加多线程中的死锁

--- src/com/koumanwei/multi/DeadLockDemo.java
+++ src/com/koumanwei/multi/DeadLockDemo.java
@@ -7,17 +7,15 @@ package com.koumanwei.multi;
  * @version 1.0
  */
 public class DeadLockDemo {
+    public static final Object LOCKA = new Object();
+    public static final Object LOCKB = new Object();
+
     public static void main(String[] args) {
-        DeadLock deadLock = new DeadLock(true);
-        Thread thread1 = new Thread(deadLock);
-        Thread thread2 = new Thread(deadLock);
+        Lock1 lock1 = new Lock1();
+        Lock2 lock2 = new Lock2();
+        Thread thread1 = new Thread(lock1);
+        Thread thread2 = new Thread(lock2);
         thread1.start();
-        try {
-            thread1.sleep(30);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        deadLock.setFlag();
         thread2.start();
     }
 }
@@ -25,26 +23,40 @@ public class DeadLockDemo {
 // 最常见的死锁情况：同步嵌套
 // 同步中还有同步，两个同步不是同一个锁
 
-class DeadLock implements Runnable {
-    private int num = 100;
-    private boolean flag;
 
-    public DeadLock(boolean flag) {
-        this.flag = flag;
-    }
+// 下面的例子说明：
+
+// 首先封装了两个线程任务lock1和lock2
+// 然后开启了两个线程分别取执行这两个任务
+// 假设lock1任务先执行了，并且拿到A锁，并打印了Lock1 A
+// 这个时候lock2也执行了，并且拿到B锁，并打印了Lock2 B
+// 此时都需要对方释放锁才能继续执行，所以变成了死锁
+class Lock1 implements Runnable {
 
     @Override
     public void run() {
-        if (flag) {
+        while (true) {
+            synchronized (DeadLockDemo.LOCKA) {
+                System.out.println(Thread.currentThread().getName() + " Lock1 A");
+                synchronized (DeadLockDemo.LOCKB) {
+                    System.out.println(Thread.currentThread().getName() + " Lock1 B");
+                }
+            }
         }
     }
+}
 
-    public void setFlag() {
-        flag = false;
-    }
+class Lock2 implements Runnable {
 
-    synchronized void show() {
-        num--;
-        System.out.println(num);
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (DeadLockDemo.LOCKB) {
+                System.out.println(Thread.currentThread().getName() + " Lock2 B");
+                synchronized (DeadLockDemo.LOCKA) {
+                    System.out.println(Thread.currentThread().getName() + " Lock2 A");
+                }
+            }
+        }
     }
 }
